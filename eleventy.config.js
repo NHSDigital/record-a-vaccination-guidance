1ddd2dc63fc23ddadbfeb455c86132bbd5572689
Frankie Roberto <frankie@frankieroberto.com>
2024-10-15 14:02:08 +0100

    Update to NHSUK Frontend version 9

--- eleventy.config.js
+++ eleventy.config.js
@@ -8,6 +8,7 @@ module.exports = function(eleventyConfig) {
   let nunjucksEnvironment = new Nunjucks.Environment(
 		new Nunjucks.FileSystemLoader([
       './node_modules/nhsuk-frontend/packages/components',
+      './node_modules/nhsuk-frontend/packages/macros',
       './node_modules/govuk-frontend/dist',
       'app/layouts',
       'app/_includes'
